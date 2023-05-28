a573cc656727b7160f2d4399fa3d6da87729d085
Alek <contact@litterpic.com>
2023-05-28 15:12:35 -0400

    Created initial community service hours page

--- pages/layout.js
+++ pages/layout.js
@@ -11,6 +11,7 @@ const Layout = ({children}) => {
                 <Navlink href="/">Home</Navlink>
                 <Navlink href="/stories">Stories</Navlink>
                 <Navlink href="/volunteer">Volunteer Events</Navlink>
+                <Navlink href="/community_service_hours">Community Service Hours</Navlink>
                 <Navlink href="/donate">Donate</Navlink>
                 <Navlink href="/about">About</Navlink>
                 <Navlink href="/contact">Contact</Navlink>
