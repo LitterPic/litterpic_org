de7e3a57d4f1ea2f6b6a8e1e930f2b5f75137778
Alek <contact@litterpic.com>
2023-05-28 13:30:53 -0400

    Updated some styling, added a logo, updates to navbar, logobar, and footer.

--- pages/layout.js
+++ pages/layout.js
@@ -1,22 +1,24 @@
 import React from "react";
-import Link from "next/link";
-import Footer from "../components/Footer";
+import Navlink from "./navlink";
+import Logobar from '../components/logobar';
+import Footer from "../components/footer";
 
-const Layout = ({ children }) => {
-  return (
-    <div>
-      <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
-        <Link href="/volunteer">Volunteer Events</Link>
-        <Link href="/donate">Donate</Link>
-        <Link href="/stories">Stories</Link>
-      </nav>
-      <main>{children}</main>
-      <Footer />
-    </div>
-  );
+const Layout = ({children}) => {
+    return (
+        <div>
+            <Logobar/>
+            <nav>
+                <Navlink href="/">Home</Navlink>
+                <Navlink href="/stories">Stories</Navlink>
+                <Navlink href="/volunteer">Volunteer Events</Navlink>
+                <Navlink href="/donate">Donate</Navlink>
+                <Navlink href="/about">About</Navlink>
+                <Navlink href="/contact">Contact</Navlink>
+            </nav>
+            <main>{children}</main>
+            <Footer/>
+        </div>
+    );
 };
 
 export default Layout;
