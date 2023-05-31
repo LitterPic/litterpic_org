b30fbe43bb5f826305e543bf5ec3f599219b00bc
Alek <contact@litterpic.com>
2023-05-31 18:43:32 -0400

    Updates to support builds

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Layout from "./layout";
-import '../styles/styles.css';
+import Layout from "../components/layout";
+import '../styles/styles.css'
 
-const MyApp = ({ Component, pageProps }) => {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+const MyApp = ({Component, pageProps}) => {
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 };
 
 export default MyApp;
