32a41d600fd3eeef93c10fb5c60bac025eca98ce
Alek <contact@litterpic.com>
2023-06-01 19:06:43 -0400

    Fixed LitterPic logo display

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 module.exports = {
     // other configurations
-    assetPrefix:
-        process.env.NODE_ENV === "production" ? "https://litterpic.org" : "",
-
-    trailingSlash: true,
-
-    images: {
-        loader: 'imgix', // use 'imgix' as loader
-        path: '', // specify the path, could be your website URL or empty
-    },
+    assetPrefix: "https://d1zode057nhvzo.cloudfront.net"
 };
