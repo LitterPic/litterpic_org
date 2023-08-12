e866a7f833a874f1cfc18cdf9e7dd485843c8f94
Alek <contact@litterpic.com>
2023-08-12 07:55:37 -0400

    LTRPICWEB-189 - Added isMigrated support for migrated user messaging and password reset.

--- components/SignInForm.js
+++ components/SignInForm.js
@@ -1,11 +1,13 @@
-import {useState, useEffect} from 'react';
-import {signInWithEmailAndPassword, onAuthStateChanged, sendPasswordResetEmail} from 'firebase/auth';
-import {auth} from '../lib/firebase';
+import {useState} from 'react';
+import {signInWithEmailAndPassword, sendPasswordResetEmail} from 'firebase/auth';
+import {collection, doc, updateDoc, query, where, getDocs, getDoc} from 'firebase/firestore';
+import {db, auth} from '../lib/firebase';
 import {useRouter} from 'next/router';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEye, faEyeSlash, faTimes} from "@fortawesome/free-solid-svg-icons";
 import {toast, ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import {debounce} from 'lodash';
 
 export default function SignInForm() {
     const [email, setEmail] = useState('');
@@ -13,22 +15,13 @@ export default function SignInForm() {
     const [showPassword, setShowPassword] = useState(false);
     const router = useRouter();
     const [error, setError] = useState('');
-    const [isEmailVerified, setIsEmailVerified] = useState(false);
+    const [isMigratedUser, setIsMigratedUser] = useState(false);
+    const [userId, setUserId] = useState(null);
+
 
     const clearError = () => {
         setError('');
     };
-
-    useEffect(() => {
-        // Check if the user's email is verified
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setIsEmailVerified(user.emailVerified);
-            }
-        });
-
-        return () => unsubscribe();
-    }, []);
 
     const togglePasswordVisibility = () => {
         setShowPassword(!showPassword);
@@ -40,6 +33,28 @@ export default function SignInForm() {
         try {
             const userCredential = await signInWithEmailAndPassword(auth, email, password);
             const user = userCredential.user;
+
+            const lastReset = user.metadata.lastPasswordReset;
+
+            if (lastReset) {
+                const recentReset = lastReset > Date.now() - PASSWORD_RESET_LIMIT;
+
+                if (recentReset) {
+                    await db.doc(`users/${user.uid}`).update({
+                        isMigrated: false
+                    });
+                }
+            }
+
+            // Get the user's document from the users collection
+            const userDocRef = doc(db, 'users', user.uid);
+            const userDoc = await getDoc(userDocRef);
+
+            // Check if the user is a migrated user
+            if (userDoc.exists() && userDoc.data().isMigrated) {
+                setError('Please reset your password.');
+                return;
+            }
 
             if (!user.emailVerified) {
                 setError('Please verify your email before logging in.');
@@ -63,16 +78,43 @@ export default function SignInForm() {
         }
     };
 
-    const handleForgotPassword = async (e) => {
+    const handleForgotPassword = async (e, userId) => {
         e.preventDefault();
         try {
             await sendPasswordResetEmail(auth, email);
             toast.success('Password reset email sent');
+
+            // Update the isMigrated flag in Firestore
+            if (isMigratedUser) {
+                const userRef = doc(db, 'users', userId);
+                await updateDoc(userRef, {isMigrated: false});
+            }
+
+            clearError();
+            setIsMigratedUser(false);
         } catch (error) {
-            console.error(error);
             toast.error('Failed to send password reset email');
         }
     };
+
+
+    const checkIfMigratedUser = async (email) => {
+        const usersRef = collection(db, 'users');
+        const q = query(usersRef, where('email', '==', email), where('isMigrated', '==', true));
+        const snapshot = await getDocs(q);
+
+        if (!snapshot.empty) {
+            setError("Your account has been migrated from LitterPic.com, Please reset your password using the Forgot Password link below.");
+            setIsMigratedUser(true);
+            const foundUserId = snapshot.docs[0].id;
+            setUserId(foundUserId);
+        } else {
+            setError('');
+            setIsMigratedUser(false);
+        }
+    };
+
+    const debouncedCheckIfMigratedUser = debounce(checkIfMigratedUser, 500);
 
     return (
         <div>
@@ -84,22 +126,46 @@ export default function SignInForm() {
                     New to LitterPic?
                     <a className="sign-in-sign-up-link" href="/signup">Sign Up</a>
                 </div>
-                <input className="sign-in-email" type="email" value={email} onChange={(e) => setEmail(e.target.value)}
-                       placeholder="Email" required/>
+                <input className="sign-in-email"
+                       type="email"
+                       value={email}
+                       onChange={async (e) => {
+                           setEmail(e.target.value);
+                           await debouncedCheckIfMigratedUser(e.target.value);
+                       }}
+                       placeholder="Email"
+                       required
+                />
                 <div className="signup-password-container">
-                    <input className="sign-in-password" type={showPassword ? 'text' : 'password'} value={password}
-                           onChange={(e) => setPassword(e.target.value)} placeholder="Password"
-                           required/>
+                    <input className="sign-in-password"
+                           type={showPassword ? 'text' : 'password'}
+                           value={password}
+                           onChange={(e) => setPassword(e.target.value)}
+                           placeholder="Password"
+                           required
+                           readOnly={isMigratedUser}
+                    />
                     <FontAwesomeIcon
                         icon={showPassword ? faEyeSlash : faEye}
                         className="signup-password-toggle-icon"
                         onClick={togglePasswordVisibility}
                     />
                 </div>
-                <a className="sign-in-forgot-password" href="#" onClick={handleForgotPassword} role="button"
-                   tabIndex="0">Forgot Password?</a>
+                <a
+                    className="sign-in-forgot-password"
+                    href="#"
+                    onClick={(e) => handleForgotPassword(e, userId)}
+                    role="button"
+                    tabIndex="0"
+                >
+                    Forgot Password?
+                </a>
 
-                <button className="sign-in-button" type="submit">Login</button>
+                <button className={`sign-in-button ${isMigratedUser ? 'disabled-button' : ''}`}
+                        type="submit"
+                        disabled={isMigratedUser}>
+                    Login
+                </button>
                 <ToastContainer/>
                 {error && (
                     <div className="error-container">
