885373ea6887a6dc396b30244b9fd5eb54565c24
Alek Babich <ababich@roadrunner.com>
2023-09-05 19:28:50 -0400

    Adding user location to visitor notification

--- pages/api/get-user-location.js
+++ pages/api/get-user-location.js
@@ -1,16 +1,7 @@
-import axios from 'axios';
-
 export default async function handler(req, res) {
-    const ipinfoAccessToken = process.env.NEXT_PUBLIC_IPINFO_ACCESS_TOKEN;
-
     try {
-        const response = await axios.get('http://ipinfo.io', {
-            headers: {
-                Authorization: `Bearer ${ipinfoAccessToken}`
-            }
-        });
-        const locationData = response.data;
-        res.status(200).json(locationData);
+        const userIPAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+        res.status(200).json({userIPAddress});
     } catch (error) {
         res.status(500).json({error: 'Internal server error'});
     }
