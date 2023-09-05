4e862e9ada637bb2061749fddf796cbc165f401c
Alek Babich <ababich@roadrunner.com>
2023-09-05 19:35:42 -0400

    Adding user location to visitor notification

--- pages/api/get-user-location.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-    try {
-        const userIPAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-        res.status(200).json({userIPAddress});
-    } catch (error) {
-        res.status(500).json({error: 'Internal server error'});
-    }
-}
