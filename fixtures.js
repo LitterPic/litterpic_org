1b9906d99f2d908bc4d489a844d5c9f484295e3d
Alek Babich <ababich@roadrunner.com>
2023-09-16 15:13:57 -0400

    LTRPICWEB-209 - removed console logs

--- fixtures.js
+++ fixtures.js
@@ -1,7 +1,6 @@
 const {test} = require('@playwright/test');
 
 test.beforeEach(async ({page}) => {
-    console.log("Custom page setup executed");
 
     // Wait for 5 seconds before trying to navigate to the page
     await page.waitForTimeout(3000);
--- pages/index.js
+++ pages/index.js
@@ -54,25 +54,21 @@ export default function Index() {
     useEffect(() => {
         async function sendNotification() {
             try {
-                console.log("Entered page");
                 // Step 1: Get the user's IP Address
                 const res = await fetch('https://api64.ipify.org?format=json');
                 const data = await res.json();
                 const ipAddress = data.ip;
-                console.log("IP", ipAddress);
 
                 // Step 2: Get Geographical Info (Example using ipstack)
                 const apiKey = process.env.NEXT_PUBLIC_IPSTACK_ACCESS_TOKEN;
                 const geoRes = await fetch(`http://api.ipstack.com/${ipAddress}?access_key=${apiKey}`);
                 const geoData = await geoRes.json();
                 const location = geoData.city + ', ' + geoData.region_name;
-                console.log("Location:", location);
 
                 // Step 3: Send SNS notification
                 const sns = new AWS.SNS();
                 const topicArn = 'arn:aws:sns:us-east-1:710280486241:litterpicOrgNewVisitor';
                 const message = `A user from ${location} has visited LitterPic.org!`;
-                console.log("Sent to AWS SNS");
 
                 const params = {
                     Message: message,
