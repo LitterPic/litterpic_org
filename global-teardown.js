300e8e1d402df94df4b228e623fa7778e1d698c7
Alek Babich <ababich@roadrunner.com>
2024-03-14 18:26:15 -0400

    LITTERPIC-176 - Fix initial Playwright implementation

--- global-teardown.js
+++ global-teardown.js
@@ -1,5 +1,18 @@
-const killPort = require('kill-port');
+const {execSync} = require('child_process');
+
+const killProcessUsingPort = (port) => {
+    try {
+        const result = execSync(`lsof -ti:${port}`).toString().trim();
+        if (result) {
+            execSync(`kill -9 ${result}`);
+            console.log(`Process on port ${port} terminated.`);
+        }
+    } catch (err) {
+        console.log(`No process to terminate on port ${port} or termination failed: ${err.message}`);
+    }
+};
 
 module.exports = async () => {
-    await killPort(3000);
+    console.log('Terminating the server process...');
+    killProcessUsingPort(3000);
 };
