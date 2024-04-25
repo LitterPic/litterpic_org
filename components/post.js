33103863ed78053b21b487ae95a5633850da5e97
Alek Babich <ababich@roadrunner.com>
2024-04-25 09:22:12 -0400

    LITTERPIC-276 - Initial support to play videos on Chrome

--- components/post.js
+++ components/post.js
@@ -1,14 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import {useSwipeable} from 'react-swipeable';
 import {FaChevronLeft, FaChevronRight} from 'react-icons/fa';
-import Link from "next/link";
+import Link from 'next/link';
 
 function Post({post}) {
     const [currentIndex, setCurrentIndex] = useState(0);
     const [userName, setUserName] = useState('');
     const [userPhoto, setUserPhoto] = useState('');
     const [isAmbassador, setIsAmbassador] = useState(false);
-    const [ambassadorDate, setAmbassadorDate] = useState(null)
+    const [ambassadorDate, setAmbassadorDate] = useState(null);
 
     useEffect(() => {
         const fetchUserInfo = async () => {
@@ -17,13 +17,11 @@ function Post({post}) {
                 setUserName(display_name || ' ');
                 setUserPhoto(photo_url || 'https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg');
             } else {
-                // Handle the case where post.user is not available
                 setUserName(' ');
                 setUserPhoto('https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg');
             }
         };
 
-        // Check if the user is an ambassador
         const ambassadorStatus = post.user.ambassador || false;
         setIsAmbassador(ambassadorStatus);
 
@@ -37,22 +35,34 @@ function Post({post}) {
     }, [post]);
 
     const nextPhoto = () => {
-        setCurrentIndex((currentIndex + 1) % post.photos.length);
+        setCurrentIndex(currentIndex => (currentIndex + 1) % post.photos.length);
     };
 
     const prevPhoto = () => {
-        setCurrentIndex((currentIndex - 1 + post.photos.length) % post.photos.length);
+        setCurrentIndex(currentIndex => (currentIndex - 1 + post.photos.length) % post.photos.length);
     };
 
     const handlers = useSwipeable({
         onSwipedLeft: nextPhoto,
         onSwipedRight: prevPhoto,
+        preventDefaultTouchmoveEvent: true,
+        trackMouse: true
     });
 
-    const hasMultiplePhotos = post.photos.length > 1;
+    const isVideo = (url) => /\.(mp4|webm)(\?|$)/i.test(url);
+
+    const renderMedia = (url, index) => {
+        return isVideo(url) ? (
+            <video key={index} controls className="carousel-image">
+                <source src={url} type="video/mp4"/>
+                Your browser does not support the video tag.
+            </video>
+        ) : (
+            <img key={index} src={url} alt="post" className="carousel-image"/>
+        );
+    };
 
     const formatDate = (timestamp) => {
-
         if (!timestamp) {
             return ' ';
         }
@@ -75,21 +85,14 @@ function Post({post}) {
         <div className="fetch-post">
             <div className="post-username-location">
                 <div className="profile-image">
-                    {userPhoto ? (<img src={userPhoto} alt="Profile"/>) : (
-                        <img
-                            src="https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg"
-                            alt="Default Profile"
-                        />
-                    )}
+                    <img src={userPhoto} alt="Profile"/>
                 </div>
-                <Link className="post-user-name" href={`/profile/${post.user.uid}`} passHref>
-                    <div>{userName}</div>
+                <Link href={`/profile/${post.user.uid}`} legacyBehavior>
+                    <a className="post-user-name">{userName}</a>
                 </Link>
                 <div className="post-location">
                     <a
-                        href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
-                            post.location
-                        )}`}
+                        href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(post.location)}`}
                         target="_blank"
                         rel="noopener noreferrer"
                     >
@@ -100,30 +103,16 @@ function Post({post}) {
                 {isAmbassador && (
                     <div className="post-ambassador">
                         <i className="material-icons post-ambassador-icon">public</i>
-                        <p className="post-ambassador-text">{`LitterPic Ambassador since
-                                ${new Date(ambassadorDate).toLocaleDateString()}`}</p>
+                        <p className="post-ambassador-text">{`LitterPic Ambassador since ${new Date(ambassadorDate).toLocaleDateString()}`}</p>
                     </div>
-
                 )}
             </div>
             <div className="post-carousel" {...handlers}>
-                {hasMultiplePhotos && (
-                    <FaChevronLeft
-                        className="carousel-chevron carousel-chevron-left"
-                        onClick={prevPhoto}
-                    />
-                )}
-                <img
-                    src={post.photos[currentIndex]}
-                    alt="post"
-                    className="carousel-image"
-                />
-                {hasMultiplePhotos && (
-                    <FaChevronRight
-                        className="carousel-chevron carousel-chevron-right"
-                        onClick={nextPhoto}
-                    />
-                )}
+                {post.photos.length > 1 &&
+                    <FaChevronLeft className="carousel-chevron carousel-chevron-left" onClick={prevPhoto}/>}
+                {renderMedia(post.photos[currentIndex], currentIndex)}
+                {post.photos.length > 1 &&
+                    <FaChevronRight className="carousel-chevron carousel-chevron-right" onClick={nextPhoto}/>}
             </div>
             <div className="post-litter-weight-collected">
                 {post.litterWeight > 0 && (
@@ -136,7 +125,6 @@ function Post({post}) {
             <div className="post-description">
                 {post.description ? post.description : 'No description available'}
             </div>
-
         </div>
     );
 }
