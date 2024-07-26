b66d323d0dcdbe5dbb8898d573a76f8e3f197e67
Alek Babich <ababich@roadrunner.com>
2024-07-26 15:00:16 -0400

    Fixed UI crash from likes on missing user

--- components/LikePopup.js
+++ components/LikePopup.js
@@ -11,17 +11,17 @@ const LikePopup = ({likedUsers}) => {
         <div className="like-list-container">
             {Array.isArray(likedUsers) && likedUsers.length > 0 && (
                 <div className="like-list">
-                    {likedUsers.map((user) => (
-                        <div key={user.id} className={`like-user ${imageLoaded ? 'loaded' : 'loading'}`}>
+                    {likedUsers.map((user, index) => (
+                        <div key={user?.id || index} className={`like-user ${imageLoaded ? 'loaded' : 'loading'}`}>
                             <div className="user-info">
                                 <img
-                                    src={user.photo_url || "https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg"}
-                                    alt={user.display_name}
+                                    src={user?.photo_url || "https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg"}
+                                    alt={user?.display_name || "Volunteer"}
                                     className="user-photo"
                                     onLoad={() => setImageLoaded(true)}
                                 />
-                                <Link href={`/profile/${user.uid}`} className="user-name">
-                                    {user.display_name}
+                                <Link href={`/profile/${user?.uid || '#'}`} className="user-name">
+                                    {user?.display_name || "Volunteer"}
                                 </Link>
                             </div>
                         </div>
