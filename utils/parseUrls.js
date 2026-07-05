b0d8f2beaea13abb0f2458dc24aa805a37e0e05b
Alek Babich <ababich@roadrunner.com>
2026-07-05 17:21:08 -0400

    Fixed comment formatting

--- utils/parseUrls.js
+++ utils/parseUrls.js
@@ -1,8 +1,10 @@
 const parseUrls = (text) => {
     const urlPattern = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    return text.replace(urlPattern, (url) => {
+    let html = text.replace(urlPattern, (url) => {
         return `<a href="${url}" target="_blank" rel="noopener noreferrer">${url}</a>`;
     });
+    // Replace newlines with <br /> to preserve formatting
+    return html.replace(/\n/g, '<br />');
 };
 
 export default parseUrls;
